e6db358af6da01f3fb8e90716d768e8568cf75c9
VolodymyrNakonechnyi <146861887+VolodymyrNakonechnyi@users.noreply.github.com>
2023-11-02 10:55:16 +0200

    Update FIPS.cpp

--- FIPS.cpp
+++ FIPS.cpp
@@ -1,203 +1,126 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 #include <string>
 #include <bitset>
-#include <ctime>
 #include <cmath>
-#include <string.h>
 #include <vector>
-
 
 std::string generateKey(int keyLength) {
     std::string keyStr = "";
 
     for (int i = 0; i < keyLength; i++) {
-        int randomBit = std::round(std::rand() / static_cast<double>(RAND_MAX));
+        int randomBit = std::rand() % 2;
         keyStr += std::to_string(randomBit);
     }
 
     return keyStr;
 }
 
-bool monobit_test(std::string key) {
-    int onebitCount = 0;
-
-    for (int i = 0; i < key.length(); i++) {
-        if (key[i] == '1') {
-            onebitCount++;
-        }
-    }
-
-    return (onebitCount < 10346 && onebitCount > 9654) ? true : false;
+bool monobit_test(const std::string& key) {
+    int onebitCount = std::count(key.begin(), key.end(), '1');
+    return (onebitCount >= 9654 && onebitCount <= 10346);
 }
 
-bool test_max_series_length(std::string key) {
-    int set = 0;
+bool test_max_series_length(const std::string& key) {
+    int maxSet = 0;
+    int currentSet = 0;
     char flag = key[0];
 
-    for (int i = 0; i < key.length(); i++) {
-        if (set > 36) {
-            std::cout << set;
-            return false;
-        }
-        
-        if (key[i] == flag) {
-            set++;
+    for (char bit : key) {
+        if (bit == flag) {
+            currentSet++;
+            maxSet = std::max(maxSet, currentSet);
         }
         else {
-            flag = key[i];
-            set = 0;
+            flag = bit;
+            currentSet = 1;
         }
     }
-    
-    return true;
+
+    return maxSet <= 36;
 }
 
-bool poker_test(std::string key) {
-    int step = 4;
-    int array[16] = { 0 };
+bool poker_test(const std::string& key) {
+    const int step = 4;
+    std::vector<int> array(16, 0);
     int k = key.length() / step;
-    
+
     for (int i = 0; i < key.length() - step; i += step) {
         int fourbit = std::stoi(key.substr(i, step), 0, 2);
-        
         array[fourbit]++;
     }
 
     int sum = 0;
-
-    for (int i = 0; i < pow(2, step); i++) {
-        sum += std::pow(array[i], 2);
+    for (int count : array) {
+        sum += count * count;
     }
 
-    float pokerCoeff = (pow(2, step) / k) * (sum) - k;
-
-    if (pokerCoeff > 1.03 && pokerCoeff < 57.04) {
-        return true;
-    }
-
-    return false;
-
+    float pokerCoeff = (16.0 / k) * sum - k;
+    return (pokerCoeff > 1.03 && pokerCoeff < 57.04);
 }
 
-
-bool check_series_lengths(std::string key) {
+bool check_series_lengths(const std::string& key) {
     int array[6] = { 0 };
     int set = 0;
-    char flag = '1';
 
-    for (int i = 0; i < key.length(); i++) {
-
-        if (key[i] == flag) {
+    for (char bit : key) {
+        if (bit == '1') {
             set++;
         }
         else {
             if (set >= 6) {
                 array[5]++;
             }
-
-            if (set != 0) {
+            else if (set != 0) {
                 array[set - 1]++;
             }
-
             set = 0;
         }
     }
 
-    for (int i = 0; i < sizeof(array) / sizeof(int); i++) {
-        int count = array[i];
+    int ranges[6][2] = { { 2267, 2733 }, { 1079, 1421 }, { 502, 748 }, { 223, 402 }, { 90, 223 }, { 90, 223 } };
 
-        switch (i) {
-        case 0:
-            if (count < 2267 || count > 2733) {
-                return false;
-            }
-
-            break;
-
-        case 1:
-            if (count < 1079 || count > 1421) {
-                return false;
-            }
-
-            break;
-
-        case 2:
-            if (count < 502 || count > 748) {
-                return false;
-            }
-
-            break;
-
-        case 3:
-            if (count < 223 || count > 402) {
-                return false;
-            }
-
-            break;
-
-        case 4:
-            if (count < 90 || count > 223) {
-                return false;
-            }
-
-            break;
-
-        case 5:
-            if (count < 90 || count >  223) {
-                return false;
-            }
-
-            break;
+    for (int i = 0; i < 6; ++i) {
+        if (array[i] < ranges[i][0] || array[i] > ranges[i][1]) {
+            return false;
         }
     }
 
     return true;
 }
 
-
-
-int main()
-{
-    std::srand(time(NULL));
-
+int main() {
+    std::srand(std::time(nullptr));
     const std::string key = generateKey(20000);
 
-    
     if (monobit_test(key)) {
-        std::cout << "Level 1: passed!" << std::endl;
+        std::cout << "Monobit test: passed!" << std::endl;
     }
     else {
-        std::cout << "Level 1: failed" << std::endl;
+        std::cout << "Monobit test: failed" << std::endl;
     }
-    
-    //+++++++++++++++++++++++++++++++++++++++++++
 
     if (test_max_series_length(key)) {
-        std::cout << "Level 2: passed!" << std::endl;
+        std::cout << "Test max series length: passed!" << std::endl;
     }
     else {
-        std::cout << "Level 2: failed" << std::endl;
+        std::cout << "Test max series length" << std::endl;
     }
-
-    //+++++++++++++++++++++++++++++++++++++++++++
 
     if (poker_test(key)) {
-        std::cout << "Level 3: passed!" << std::endl;
+        std::cout << "Poker test: passed!" << std::endl;
     }
     else {
-        std::cout << "Level 3: failed" << std::endl;
+        std::cout << "Poker test: failed" << std::endl;
     }
-
-    //+++++++++++++++++++++++++++++++++++++++++++
 
     if (check_series_lengths(key)) {
-        std::cout << "Level 4: passed!" << std::endl;
+        std::cout << "Check series lengths test: passed!" << std::endl;
     }
     else {
-        std::cout << "Level 4: failed" << std::endl;
+        std::cout << "Check series lengths test: failed" << std::endl;
     }
-
 
     return 0;
 }
